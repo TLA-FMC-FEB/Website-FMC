84e5c0bc7b118f4dce6be1fbc7bd5df897df6eba
Hinjikani <pascalhanif@gmail.com>
2024-10-04 20:32:38 +0700

    Removing unnecessary comment

--- static/script/showVideo.js
+++ static/script/showVideo.js
@@ -5,10 +5,6 @@ function showVideo(video) {
         content.innerHTML = `
             <lite-youtube videoid="fpLwzsZPx5A" playlabel="Konsep IPO"></lite-youtube>
         `;
-        // videoContent = document.createElement('lite-youtube');
-        // videoContent.setAttribute('videoid', 'ogfYd705cRs');
-        // videoContent.setAttribute('playlabel', 'Keynote');
-        // content.appendChild(videoContent);
     } else if (video === 'pasarmodal') {
         content.innerHTML = `
             <lite-youtube videoid="4sP-PC8O6SI" playlabel="Pasar Modal"></lite-youtube>
