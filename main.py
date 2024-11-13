b661feb9b20710c748e588e6db7d354c17d8d74d
Hinjikani <pascalhanif@gmail.com>
2024-11-13 12:01:35 +0700

    Reversing minor adjustment push

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+    
+@app.route('/about')
+def about():
+    return render_template("about-us.html")
+    
+@app.route('/events')
+def events():
+    return render_template("events.html")
+
+@app.route('/learn')
+def learn():
+    return render_template("learn.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
